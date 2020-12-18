5f1ed50a8fb56518e798be7d318e8ff6c0bc3dd8
Pavel Romash <pashar@tut.by>
2020-12-18 11:44:01 +0300

    Add project files.

---
+++ NUnitTestProjectWithGit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTestProjectWithGit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
